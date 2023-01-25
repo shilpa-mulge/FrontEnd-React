1e036fc208e7a6685c8e3abc07918ee8ab24dd1d
Shilpa <mulgeshilpa@gmail.com>
2023-01-25 19:20:13 +0530

    ExpenseItem

---
+++ src/components/ExpenseItems.js
@@ -0,0 +1,11 @@
+function ExpenseItem(){
+    return(
+        <div>
+        <h2>Expense item!</h2>
+        <div>Food Rs 10</div>
+        <div>Petrol Rs 100</div>
+        <div>Movies Rs 200</div>
+        </div>
+    )
+}
+export default ExpenseItem;
