168de5390bc26e9ba3757c2c12bf1748fbcf737c
Shilpa <mulgeshilpa@gmail.com>
2023-01-30 16:03:47 +0530

    Implementing the filter feature

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -6,22 +6,32 @@ import ExpenseFilter from './ExpenseFilter';
 const Expenses=(props)=>{
     const [Userselected, setUserSelected]=useState('2020');
     const onUserSelected=(userSelectedYear)=>{
-  setUserSelected(userSelectedYear);
-    }
+        setUserSelected(userSelectedYear);
+        }
+        const filterdExpenses=props.items.filter(expense=>{
+            return expense.date.getFullYear().toString()===Userselected;
+        })
+
+
 return (
     <div>
     <Card className="expenses">
     <ExpenseFilter selected={Userselected} onSelected={onUserSelected}/>
-{ props.items.map((exp)=>{
-return    <ExpenseItem
+  {  filterdExpenses.map((exp)=>(
+
+<ExpenseItem
+
 key={exp.id}
+
 title={exp.title}
- amount={exp.amount}
-  date={exp.date}>
-  </ExpenseItem>
 
+amount={exp.amount}
 
-})}
+date={exp.date}>
+
+</ExpenseItem>
+
+))}
 </Card>
 </div>
 )
