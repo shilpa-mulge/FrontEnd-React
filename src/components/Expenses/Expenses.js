9d0e73292f01188874d8f0c1bdcc76f1975b040e
Shilpa <mulgeshilpa@gmail.com>
2023-01-31 09:48:22 +0530

    Chart App

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,9 @@
 import './Expenses.css';
+import ExpenseChart from './ExpensesChart';
 import React,{useState} from 'react';
 import Card from '../UI/Card';
-import ExpenseItem from "./ExpenseItems";
 import ExpenseFilter from './ExpenseFilter';
+import ExpenseList from './ExpenseList';
 const Expenses=(props)=>{
     const [Userselected, setUserSelected]=useState('2020');
     const onUserSelected=(userSelectedYear)=>{
@@ -11,27 +12,12 @@ const Expenses=(props)=>{
         const filterdExpenses=props.items.filter(expense=>{
             return expense.date.getFullYear().toString()===Userselected;
         })
-
-
 return (
     <div>
     <Card className="expenses">
     <ExpenseFilter selected={Userselected} onSelected={onUserSelected}/>
-  {  filterdExpenses.map((exp)=>(
-
-<ExpenseItem
-
-key={exp.id}
-
-title={exp.title}
-
-amount={exp.amount}
-
-date={exp.date}>
-
-</ExpenseItem>
-
-))}
+<ExpenseChart expenses={filterdExpenses}/>
+<ExpenseList items={filterdExpenses}/>
 </Card>
 </div>
 )
